90b6b86f1edb15b013da1f65232c459a34eeaedd
Yehia Ehab <yehiab07@gmail.com>
2026-02-04 14:16:37 +0400

    switch to svg

---
+++ images/svg.js
@@ -0,0 +1,18 @@
+const globeIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg>`;
+const copyIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>`;
+const infoIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 13h.01"/><path d="M12 6v3"/><path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20"/></svg>`;
+
+const globeContainer = document.getElementById("translate-icon");
+if (globeContainer) {
+  globeContainer.innerHTML = globeIcon;
+}
+
+const copyContainer = document.getElementById("copy-icon");
+if (copyContainer) {
+  copyContainer.innerHTML = copyIcon;
+}
+
+const infoContainer = document.getElementById("information-icon");
+if (infoContainer) {
+  infoContainer.innerHTML = infoIcon;
+}
